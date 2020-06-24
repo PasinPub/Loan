f454f8458f9b628e852be9e1f0f780bda77f5f9a
PasinPub <pasin.pubpateeravanich@scb.co.th>
2020-06-24 15:10:40 +0700

    commited

---
+++ src/main/java/com/digitalacademy/loan/Application.java
@@ -0,0 +1,14 @@
+package com.digitalacademy.loan;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class Application {
+    public static void main(String[] argh){
+        SpringApplication.run(Application.class, argh);
+    }
+}
+
